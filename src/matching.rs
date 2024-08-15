eb43169d8c87e0295caab972c7cc94b2f79abc7e
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-08-15 15:53:02 +0200

    drop the get_best_match function
    And add an helper function try_match_appid instead

--- src/matching.rs
+++ src/matching.rs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use crate::DesktopEntry;
-use log::debug;
-use std::cmp::max;
 
 impl<'a> DesktopEntry<'a> {
     /// The returned value is between 0.0 and 1.0 (higher value means more similar).
@@ -101,162 +99,21 @@ impl<'a> DesktopEntry<'a> {
             .max_by(|e1, e2| e1.total_cmp(e2))
             .unwrap_or(0.0)
     }
-}
-fn compare_str<'a>(pattern: &'a str, de_value: &'a str) -> f64 {
-    let lcsstr = textdistance::str::lcsstr(pattern, de_value);
 
-    lcsstr as f64 / (max(pattern.len(), de_value.len())) as f64
-}
+    /// Return true if the appid provided match this [`DesktopEntry`].
+    pub fn try_match_appid(&'a self, appid: &str) -> bool {
+        let normalized_appid = appid.to_lowercase();
 
-/// From 0 to 1.
-/// 1 is a perfect match.
-fn match_entry_from_id(pattern: &str, de: &DesktopEntry) -> f64 {
-    // (pattern, malus)
-    let mut de_inputs = Vec::with_capacity(4);
-
-    let id = de.appid.to_lowercase();
-
-    if let Some(last_part_of_id) = id.split('.').last() {
-        de_inputs.push((last_part_of_id.to_owned(), 0.06));
-    }
-
-    de_inputs.push((id, 0.));
-
-    if let Some(i) = de.startup_wm_class() {
-        de_inputs.push((i.to_lowercase(), 0.));
-    }
-
-    if let Some(i) = de.exec() {
-        de_inputs.push((i.to_lowercase(), 0.06));
-    }
-
-    de_inputs
-        .iter()
-        .map(|de| (compare_str(pattern, &de.0) - de.1).max(0.))
-        .max_by(|e1, e2| e1.total_cmp(e2))
-        .unwrap_or(0.0)
-}
-
-#[derive(Debug, Clone)]
-pub struct MatchAppIdOptions {
-    /// Minimal score required to validate a match.
-    /// Must be between 0 and 1
-    pub min_score: f64,
-    /// Optional field to lower the minimal score required to match
-    /// if the entropy is at a certain value, e.i if the two best matches
-    /// hare very different.
-    /// - First element - minimal entropy, between 0 and 1
-    /// - Second element - minimal score, between 0 and 1
-    pub entropy: Option<(f64, f64)>,
-}
-
-impl Default for MatchAppIdOptions {
-    fn default() -> Self {
-        Self {
-            min_score: 0.15,
-            entropy: Some((0.15, 0.1)),
+        if self.appid.to_lowercase() == normalized_appid {
+            return true;
         }
-    }
-}
 
-/// Return the best match over all provided [`DesktopEntry`].
-/// Use this to match over the values provided by the compositor, not the user.
-/// First entries get the priority.
-pub fn get_best_match<'a, I>(
-    patterns: &[I],
-    entries: &'a [DesktopEntry<'a>],
-    options: MatchAppIdOptions,
-) -> Option<&'a DesktopEntry<'a>>
-where
-    I: AsRef<str>,
-{
-    let mut max_score = None;
-    let mut second_max_score = 0.;
-
-    let normalized_patterns = patterns
-        .iter()
-        .map(|e| e.as_ref().to_lowercase())
-        .inspect(|e| {
-            debug!("searching with {}", e);
-        })
-        .collect::<Vec<_>>();
-
-    for de in entries {
-        let score = normalized_patterns
-            .iter()
-            .map(|p| match_entry_from_id(p, de))
-            .max_by(|e1, e2| e1.total_cmp(e2))
-            .unwrap_or(0.0);
-
-        match max_score {
-            Some((prev_max_score, _)) => {
-                if prev_max_score < score {
-                    debug!(
-                        "found {} for {}. Score: {}",
-                        de.appid,
-                        patterns[0].as_ref(),
-                        score
-                    );
-                    second_max_score = prev_max_score;
-                    max_score = Some((score, de));
-                }
-            }
-            None => {
-                debug!(
-                    "found: {} for {}. Score: {}",
-                    de.appid,
-                    patterns[0].as_ref(),
-                    score
-                );
-                max_score = Some((score, de));
+        if let Some(field) = self.startup_wm_class() {
+            if field.to_lowercase() == normalized_appid {
+                return true;
             }
         }
 
-        if score > 0.99 {
-            break;
-        }
-    }
-
-    if let Some((max_score, de)) = max_score {
-        if max_score > options.min_score {
-            Some(de)
-        } else if let Some((min_entropy, min_score_entropy)) = options.entropy {
-            let entropy = max_score - second_max_score;
-
-            if entropy > min_entropy && max_score > min_score_entropy {
-                Some(de)
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    } else {
-        None
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::{default_paths, get_languages_from_env, matching::compare_str, DesktopEntry, Iter};
-
-    use super::{get_best_match, MatchAppIdOptions};
-
-    #[test]
-    fn find_de() {
-        let entries =
-            DesktopEntry::from_paths(Iter::new(default_paths()), Some(&get_languages_from_env()))
-                .filter_map(|e| e.ok())
-                .collect::<Vec<_>>();
-
-        let e = get_best_match(&["gnome-disks"], &entries, MatchAppIdOptions::default());
-
-        println!("found {}", e.unwrap().appid);
-    }
-    #[test]
-    fn a() {
-        let res = compare_str("org.gnome.tweaks", "gnome.disks");
-
-        println!("{res}")
+        false
     }
 }
